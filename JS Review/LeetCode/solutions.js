db549bf98b444915eb46de1773ca4fc81231bdf6
Jarrod Bogard <jarrodbogard@gmail.com>
2024-09-13 08:02:40 -0400

    add merge solutions

--- JS Review/LeetCode/solutions.js
+++ JS Review/LeetCode/solutions.js
@@ -27,3 +27,111 @@ var merge = function (nums1, m, nums2, n) {
     k--;
   }
 };
+
+// 27. Remove Element
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+// Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+// Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+
+// Return k.
+
+var removeElement = function (nums, val) {
+  let k = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i];
+    }
+  }
+  return k;
+};
+
+// 26. Remove Duplicates from Sorted Array
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+// Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+// Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+
+// Return k.
+
+var removeDuplicates = function (nums) {
+  if (nums.length === 0) return 0;
+  let k = 0;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[k]) {
+      k++;
+      nums[k] = nums[i];
+    }
+  }
+  return k + 1;
+};
+
+// 80. Remove Duplicates from Sorted Array II
+// Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
+
+// Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+// Return k after placing the final result in the first k slots of nums.
+
+// Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+var removeDuplicates = function (nums) {
+  if (nums.length <= 2) return nums.length;
+
+  let k = 2;
+
+  for (let i = 2; i < nums.length; i++) {
+    if (nums[i] !== nums[k - 2]) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+  return k;
+};
+
+// 169. Majority Element
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+var majorityElement = function (nums) {
+  let candidate;
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (count === 0) {
+      candidate = nums[i];
+    }
+
+    if (nums[i] === candidate) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+
+  return candidate;
+};
+
+// 189. Rotate Array
+// Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+function rotate(nums, k) {
+  let n = nums.length;
+  k = k % n; // Normalize k in case k > n
+
+  for (let i = 0; i < k; i++) {
+    // Save the last element
+    let last = nums[n - 1];
+
+    // Shift all elements to the right
+    for (let j = n - 1; j > 0; j--) {
+      nums[j] = nums[j - 1];
+    }
+
+    // Place the saved last element at the front
+    nums[0] = last;
+  }
+}
